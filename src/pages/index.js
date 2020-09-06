5794041bba9366029a9a4dab3b2238bb7444c78e
Aidan Brown <aidanbrown87@gmail.com>
2020-09-06 22:12:26 +0100

    removes rogue console.log

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,6 @@ import ImageCarousel from '../components/ImageCarousel'
 class BlogIndex extends React.Component {
   formatCarouselImages() {
     return get(this, 'props.data.imagesHero.edges').map(node => {
-      console.log(node.node.frontmatter)
       return {
         ...node.node.frontmatter.heroImage.childImageSharp.fluid,
         position: node.node.frontmatter.heroImagePosition,
